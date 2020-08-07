6aa31fd18f24ac607ba3f7d802c02ce61e6c12c4
yangkeke <a616560963@126.com>
2020-08-07 14:57:35 +0800

    为了删除做准备

---
+++ src/main/java/com/test1.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * @Author: York
+ * @Date: 2020/8/4 16:29
+ * @Description: test1
+ */
+public class test1 {
+
+
+
+    public static void main(String[] args) {
+        String s=new String("mijjj");
+        System.out.println(s);
+    }
+}
---
+++ src/main/java/java/lang/String.java
@@ -0,0 +1,18 @@
+package java.lang;
+
+/**
+ * @Author: York
+ * @Date: 2020/8/4 16:28
+ * @Description: String
+ */
+public class String {
+    static {
+        System.out.println(
+                " 大家好"
+        );
+    }
+
+    public static void main(String[] args) {
+        System.out.println("我不好");
+    }
+}
