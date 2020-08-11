fae86c2240275551a84f276d88967a825d40ecb0
york <a616560963@gmail.com>
2020-08-11 10:00:53 +0800

    NC

---
+++ src/main/java/com/ResourcesStock.java
@@ -0,0 +1,30 @@
+package com;
+
+public class ResourcesStock {
+    private int num;
+    private String name;
+
+    public ResourcesStock() {
+    }
+
+    public ResourcesStock(int num, String name) {
+        this.num = num;
+        this.name = name;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
